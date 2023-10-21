c5348cea7c4f995b2ab0fc410da7d6db5a9bf3ef
QC20 <xgv866@alumni.ku.dk>
2023-10-21 04:06:01 +0200

    TO DO Future Refractor (files are currently not used)

---
+++ script.js
@@ -0,0 +1,17 @@
+// refractor in the future 
+
+function init() {
+    // ...
+}
+
+function initializeDrag(disk, e) {
+    // ...
+}
+
+function dragDisk(event) {
+    // ...
+}
+
+function dropDisk(disk) {
+    // ...
+}
